36e32b7a45a089b10ed76b6751a87944c63523c7
Gurpreet Singh <gurpreet.singh1@agreeya.net>
2020-01-09 23:47:39 +0530

    style-loader added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-import {hello} from "./hello";
+import CSR from "../assets/CSR.png";
+import "./style.scss";
+import("./module-1").then(mod => {
+  const nothing = mod.default();
+  const nothingToo = mod.useless();
 
-hello("Webpack Basics!");
+  // logs "This function does nothing and neither this one!"
+  console.log(`${nothing} and ${nothingToo}`);
+});
+
+document.body.innerHTML = '<div id="myMemes"></div>';
+document.getElementById("myMemes").innerHTML = `
+  <img src="${CSR}" />
+`;
